668e0c7199030e8aa3a7f3811cbb7375d823bd21
Sumit Gupta <summitgupta07@gmail.com>
2024-01-19 12:30:48 +0530

    gitup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
 // import link
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // import logo
-import Logo from '../assets/img/logo.svg';
+import Logo from "../assets/img/logo.svg";
 
 const Header = () => {
   return (
-    <header className='py-6 mb-12 border-b'>
-      <div className='container mx-auto flex justify-between items-center'>
-        <Link to='/'>
-          <img src={Logo} alt='' />
+    <header className="py-6 mb-12 border-b">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/">
+          <img src={Logo} alt="" />
         </Link>
-        <div className='flex items-center gap-6'>
-          <Link className='hover:text-violet-900 transition' to='/'>
+        <div className="flex items-center gap-6">
+          <Link className="hover:text-violet-900 transition" to="/">
             Log in
           </Link>
           <Link
-            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
-            to='/'
+            className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition"
+            to="/"
           >
-            Sign up
+            Sign Up
           </Link>
         </div>
       </div>
